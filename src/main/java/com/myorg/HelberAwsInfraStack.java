c2bf2d92def28a15e3d7ae1df0946d949db73bcf
Helber Luz <luzhelber@gmail.com>
2023-06-19 14:32:48 -0300

    Update HelberAwsInfraStack.java

--- src/main/java/com/myorg/HelberAwsInfraStack.java
+++ src/main/java/com/myorg/HelberAwsInfraStack.java
@@ -1,24 +1,55 @@
+@@ -1,15 +1,10 @@
 package com.myorg;
 
-import software.constructs.Construct;
+import software.amazon.awscdk.Fn;
+import software.amazon.awscdk.RemovalPolicy;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-// import software.amazon.awscdk.Duration;
-// import software.amazon.awscdk.services.sqs.Queue;
+import software.amazon.awscdk.*;
+import software.amazon.awscdk.services.applicationautoscaling.EnableScalingProps;
+import software.amazon.awscdk.services.ecr.IRepository;
+import software.amazon.awscdk.services.ecr.Repository;
+import software.amazon.awscdk.services.ecs.AwsLogDriverProps;
+import software.amazon.awscdk.services.ecs.Cluster;
+import software.amazon.awscdk.services.ecs.ContainerImage;
+import software.amazon.awscdk.services.ecs.LogDriver;
+import software.amazon.awscdk.services.ecs.*;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
+import software.amazon.awscdk.services.logs.LogGroup;
+@@ -33,11 +28,11 @@ public AluraServiceStack(final Construct scope, final String id, final StackProp
 
-public class HelberAwsInfraStack extends Stack {
-    public HelberAwsInfraStack(final Construct scope, final String id) {
-        this(scope, id, null);
-    }
+        IRepository iRepository = Repository.fromRepositoryName(this, "repositorio", "img-pedidos-ms");
 
-    public HelberAwsInfraStack(final Construct scope, final String id, final StackProps props) {
-        super(scope, id, props);
+        ApplicationLoadBalancedFargateService.Builder.create(this, "AluraService")
+        ApplicationLoadBalancedFargateService aluraService = ApplicationLoadBalancedFargateService.Builder.create(this, "AluraService")
+                .serviceName("alura-service-ola")
+                .cluster(cluster)           // Required
+                .cpu(512)                   // Default is 256
+                .desiredCount(3)            // Default is 1
+                .desiredCount(1)            // Default is 1
+                .listenerPort(8080)
+                .assignPublicIp(true)
+                .taskImageOptions(
+@@ -57,5 +52,21 @@ public AluraServiceStack(final Construct scope, final String id, final StackProp
+                .memoryLimitMiB(1024)       // Default is 512
+                .publicLoadBalancer(true)   // Default is false
+                .build();
 
-        // The code that defines your stack goes here
+        ScalableTaskCount scalableTarget = aluraService.getService().autoScaleTaskCount(EnableScalingProps.builder()
+                .minCapacity(1)
+                .maxCapacity(3)
+                .build());
 
-        // example resource
-        // final Queue queue = Queue.Builder.create(this, "HelberAwsInfraQueue")
-        //         .visibilityTimeout(Duration.seconds(300))
-        //         .build();
+        scalableTarget.scaleOnCpuUtilization("CpuScaling", CpuUtilizationScalingProps.builder()
+                .targetUtilizationPercent(70)
+                .scaleInCooldown(Duration.minutes(3))
+                .scaleOutCooldown(Duration.minutes(2))
+                .build());
+        scalableTarget.scaleOnMemoryUtilization("MemoryScaling", MemoryUtilizationScalingProps.builder()
+                .targetUtilizationPercent(65)
+                .scaleInCooldown(Duration.minutes(3))
+                .scaleOutCooldown(Duration.minutes(2))
+                .build());
     }
 }
